a3a8c9cebd00fbec4ee1ed25a3abc48a5990b801
Thomas Terosiet <thomas.terosiet@epitech.eu>
2024-12-14 18:01:16 +0100

    Initial Commit

---
+++ VIP_Sample/VIP_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIP_SampleApp.swift
+//  VIP_Sample
+//
+//  Created by Thomas TEROSIET on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct VIP_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VIP_SampleTests/VIP_SampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  VIP_SampleTests.swift
+//  VIP_SampleTests
+//
+//  Created by Thomas TEROSIET on 14/12/2024.
+//
+
+import Testing
+@testable import VIP_Sample
+
+struct VIP_SampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
